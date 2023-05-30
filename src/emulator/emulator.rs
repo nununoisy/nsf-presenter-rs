751eb761f785b44b3764b52208fb1b077a5fdd17
Noah Sweilem <nsweilem@vt.edu>
2023-05-29 19:22:55 -0700

    Proper loop detection for VRC7 modules

--- src/emulator/emulator.rs
+++ src/emulator/emulator.rs
@@ -156,6 +156,9 @@ impl Emulator {
 
     fn get_famitracker_song_position(&self, mut ptr: usize) -> SongPosition {
         if let Some(nsf) = &self.nsf {
+            if nsf.vrc7() {
+                ptr += 1;
+            }
             if nsf.fds() {
                 ptr += 2;
             }
