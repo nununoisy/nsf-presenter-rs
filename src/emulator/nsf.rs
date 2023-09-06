be3ba8a87d308fd2f87e9af04171dfe0e628b0bf
Noah Sweilem <nsweilem@vt.edu>
2023-09-06 13:43:08 -0700

    Add support for Shift-JIS encoded NSF metadata

--- src/emulator/nsf.rs
+++ src/emulator/nsf.rs
@@ -1,5 +1,6 @@
 use std::str;
 use crate::emulator::nsfeparser::NsfeMetadata;
+use encoding_rs::{CoderResult, SHIFT_JIS};
 
 pub fn find_subsequence<T>(haystack: &[T], needle: &[T]) -> Option<usize>
     where for<'a> &'a [T]: PartialEq
@@ -34,6 +35,18 @@ fn determine_driver_type(raw_bytes: &[u8]) -> NsfDriverType {
     }
 }
 
+pub fn decode_shift_jis(s: &[u8]) -> Option<String> {
+    let mut decoder = SHIFT_JIS.new_decoder();
+    let mut result = String::new();
+    result.reserve(s.len() * 4);  // Probably way more than ever needed but better safe than sorry
+
+    let (coder_result, _bytes_read, did_replacements) = decoder.decode_to_string(s, &mut result, true);
+    if coder_result == CoderResult::OutputFull || did_replacements {
+        return None;
+    }
+
+    Some(result)
+}
 
 macro_rules! string_fn {
     ($name: tt, $offset: literal, $max_len: literal) => {
@@ -92,6 +105,9 @@ impl Nsf {
             .position(|i| self.raw_bytes[i] == 0)
             .unwrap_or(max_len);
 
+        if let Some(shift_jis) = decode_shift_jis(&self.data[offset..offset+end]) {
+            return Ok(shift_jis);
+        }
         match str::from_utf8(&self.raw_bytes[offset..offset+end]) {
             Ok(s) => Ok(s.to_string()),
             Err(e) => Err(e.to_string())
